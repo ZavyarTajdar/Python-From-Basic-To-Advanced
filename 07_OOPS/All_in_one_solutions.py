0bf918c148e6714bf6042add3626a1dcc49ddb09
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-12-17 01:08:39 +0500

    Refactor Car class: improve structure and add fuel_type method

--- 07_OOPS/All_in_one_solutions.py
+++ 07_OOPS/All_in_one_solutions.py
@@ -1,14 +1,23 @@
 # ************************************Solution: 1*******************************
 class Car:
+    total_car = 0
+
     def __init__(self, userbrand, usermodel):
         self.__brand = userbrand  # Kinda Private
         self.model = usermodel
+        Car.total_car += 1
     
-    def get_brand(self):
+    def get_brand(self): # Getter
         return self.__brand+ " !"
+    
+    def set_brand(self, new_brand): # Setter
+        self.__brand = new_brand
 
     def full_name(self):
         return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self): # Polymorphism
+        return "Petrol Or Diesel"
 
 # my_car = Car("Toyota", "Corolla")
 # print(my_car.brand, my_car.model)
@@ -22,8 +31,12 @@ class Electri_Car(Car):
     def __init__(self, userbrand, usermodel, battery_size):
         super().__init__(userbrand, usermodel)
         self.battery_size = battery_size
+    def fuel_type(self): # Polymorphism
+        return "Electric Charge"
 
 my_eCar = Electri_Car("Tesla", "S", "85kWh")
 print(my_eCar.get_brand())
 
-
+safari = Car("Tata", "Safari")
+print(safari.fuel_type())
+print(Car.total_car)
