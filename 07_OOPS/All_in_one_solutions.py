c352725a5e94f4675e517ad94c679cda9a3639b7
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2025-12-17 01:32:20 +0500

    Fix Car class model attribute handling and enhance encapsulation

--- 07_OOPS/All_in_one_solutions.py
+++ 07_OOPS/All_in_one_solutions.py
@@ -4,7 +4,7 @@ class Car:
 
     def __init__(self, userbrand, usermodel):
         self.__brand = userbrand  # Kinda Private
-        self.model = usermodel
+        self.__model = usermodel # read-Only
         Car.total_car += 1
     
     def get_brand(self): # Getter
@@ -14,10 +14,19 @@ class Car:
         self.__brand = new_brand
 
     def full_name(self):
-        return f"{self.__brand} {self.model}"
+        return f"{self.__brand} {self.__model}"
     
     def fuel_type(self): # Polymorphism
         return "Petrol Or Diesel"
+    
+    @staticmethod
+    def general_description():
+        return "Cars are means of transport"
+
+    @property # Read-only decorator
+    def model(self):
+        return self.__model
+
 
 # my_car = Car("Toyota", "Corolla")
 # print(my_car.brand, my_car.model)
@@ -35,8 +44,31 @@ class Electri_Car(Car):
         return "Electric Charge"
 
 my_eCar = Electri_Car("Tesla", "S", "85kWh")
-print(my_eCar.get_brand())
 
-safari = Car("Tata", "Safari")
-print(safari.fuel_type())
-print(Car.total_car)
+# print(isinstance(my_eCar, Car))
+# print(isinstance(my_eCar, Electri_Car))
+
+# print(my_eCar.get_brand())
+# 
+# safari = Car("Tata", "Safari")
+# print(safari.fuel_type())
+# print(Car.total_car)
+
+# print(Car.general_description())
+
+
+class Battery: 
+    def batteryInfo(self):
+        return "High Power Batter"
+
+class Engine:
+    def EngineInfo(self):
+        return "High Power Engine"
+    
+
+class E_Car(Battery, Engine, Car):
+    pass
+
+new_carr = E_Car("Kia", "Carnival")
+print(new_carr.EngineInfo())
+print(new_carr.batteryInfo())
